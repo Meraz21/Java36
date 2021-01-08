8ca7d91bff1e5b3619acce5820a5e861df5564fd
Meraz21 <Meraz21>
2021-01-07 20:18:31 -0500

    Rehan and Sama has added

---
+++ src/pac/pro1.java
@@ -0,0 +1,34 @@
+package pac;
+
+import java.util.ArrayList;
+
+public class pro1 {
+public static void main(String[] args) {
+	String [] name = {"Khosruz", "Zaman", "Meraz", "Liza", "Amma", "Abba", "Vai", "Sister", "Anika", 
+			"Rehan", "Shama"};
+	System.out.println("Available name is: ");
+	for (String lis : name) {
+		System.out.print(lis+", ");
+	}
+	ArrayList allname = new ArrayList();
+	allname.add("khosru");
+	allname.add("foyshal");
+	allname.add("lili");
+	allname.add("bably");
+	allname.add("lovely");
+	allname.add("anika");
+	allname.add("shontul");
+	allname.add("amma");
+	allname.add("abba");
+	allname.add("fatima");
+	allname.add("liza");
+	allname.add("Meraz");
+	System.out.println("\nAvailable List name is : ");
+	for (Object listname : allname) {
+		System.out.print(listname+", ");
+		
+	}
+	
+	
+}
+}
